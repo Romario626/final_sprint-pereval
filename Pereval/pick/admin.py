cf6b43bcdf99876315ab0e10569320ebc9feb727
Romario626 <131347697+Romario626@users.noreply.github.com>
2023-11-04 14:51:30 +0300

    Delete Pereval directory

--- Pereval/pick/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Coords, Image, Level, Pereval
-
-admin.site.register(User)
-admin.site.register(Coords)
-admin.site.register(Image)
-admin.site.register(Level)
-admin.site.register(Pereval)
-# Register your models here.
